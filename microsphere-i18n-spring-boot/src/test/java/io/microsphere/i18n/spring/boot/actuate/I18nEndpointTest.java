7f41515170485c7a0d354c8594823fde46569703
Mercy Ma <mercyblitz@gmail.com>
2025-11-20 19:55:46 +0800

    Add author tag to I18nEndpointTest JavaDoc
    Included '@author li.qi' in the JavaDoc of I18nEndpointTest for proper attribution.

--- microsphere-i18n-spring-boot/src/test/java/io/microsphere/i18n/spring/boot/actuate/I18nEndpointTest.java
+++ microsphere-i18n-spring-boot/src/test/java/io/microsphere/i18n/spring/boot/actuate/I18nEndpointTest.java
@@ -25,6 +25,7 @@ import static org.hamcrest.Matchers.equalTo;
 /**
  * {@link I18nEndpoint} Test
  *
+ * @author li.qi
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see I18nEndpoint
  * @since 1.0.0
