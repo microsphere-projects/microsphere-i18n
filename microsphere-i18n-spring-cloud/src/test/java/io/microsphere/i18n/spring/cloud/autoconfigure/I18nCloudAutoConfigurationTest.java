334b5e10ebe881be477e984784415dc5ed6cff7e
Mercy Ma <mercyblitz@gmail.com>
2025-11-20 19:53:23 +0800

    Refactor I18nCloudAutoConfigurationTest imports and formatting
    Replaces direct import of AutoConfigurations with static import of 'of', adds class-level Javadoc, and improves code formatting for readability in I18nCloudAutoConfigurationTest.

--- microsphere-i18n-spring-cloud/src/test/java/io/microsphere/i18n/spring/cloud/autoconfigure/I18nCloudAutoConfigurationTest.java
+++ microsphere-i18n-spring-cloud/src/test/java/io/microsphere/i18n/spring/cloud/autoconfigure/I18nCloudAutoConfigurationTest.java
@@ -4,25 +4,33 @@ import io.microsphere.i18n.spring.boot.autoconfigure.I18nAutoConfiguration;
 import io.microsphere.i18n.spring.cloud.event.ReloadableResourceServiceMessageSourceListener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.boot.autoconfigure.AutoConfigurations.of;
 
-
+/**
+ * {@link I18nCloudAutoConfiguration} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see I18nCloudAutoConfiguration
+ * @since 1.0.0
+ */
 class I18nCloudAutoConfigurationTest {
-    ApplicationContextRunner applicationContextRunner ;
+
+    ApplicationContextRunner applicationContextRunner;
 
     @BeforeEach
     void setup() {
         applicationContextRunner = new ApplicationContextRunner()
-                .withConfiguration(AutoConfigurations.of(I18nAutoConfiguration.class, I18nCloudAutoConfiguration.class));
+                .withConfiguration(of(I18nAutoConfiguration.class, I18nCloudAutoConfiguration.class));
     }
 
     @Test
     void shouldContainReloadableResourceServiceMessageSourceListenerBean() {
-        applicationContextRunner.run(context -> assertThat(context).hasSingleBean(ReloadableResourceServiceMessageSourceListener.class));
+        applicationContextRunner.run(context ->
+                assertThat(context).hasSingleBean(ReloadableResourceServiceMessageSourceListener.class));
     }
 
     @Test
