b1a8a5e732c4cd6541e7bb98d9a75b29737fb566
Mercy Ma <mercyblitz@gmail.com>
2025-11-20 19:52:21 +0800

    Refactor test to use static import for AutoConfigurations
    Replaces direct reference to AutoConfigurations.of with a static import in I18nAutoConfigurationTests. Also adds class-level Javadoc for improved documentation.

--- microsphere-i18n-spring-boot/src/test/java/io/microsphere/i18n/spring/boot/autoconfigure/I18nAutoConfigurationTests.java
+++ microsphere-i18n-spring-boot/src/test/java/io/microsphere/i18n/spring/boot/autoconfigure/I18nAutoConfigurationTests.java
@@ -8,12 +8,19 @@ import io.microsphere.i18n.spring.context.I18nApplicationListener;
 import io.microsphere.i18n.spring.context.MessageSourceAdapter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.boot.autoconfigure.AutoConfigurations.of;
 
+/**
+ * {@link I18nAutoConfiguration} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see I18nAutoConfiguration
+ * @since 1.0.0
+ */
 class I18nAutoConfigurationTests {
 
     ApplicationContextRunner applicationContextRunner;
@@ -26,13 +33,12 @@ class I18nAutoConfigurationTests {
     @Test
     void shouldContainServiceMessageSourceFactoryBean() {
         applicationContextRunner.withPropertyValues("spring.application.name=I18nAutoConfigurationTests")
-                .withConfiguration(AutoConfigurations.of(I18nAutoConfiguration.class, PropertyPlaceholderAutoConfiguration.class))
+                .withConfiguration(of(I18nAutoConfiguration.class, PropertyPlaceholderAutoConfiguration.class))
                 .run(context -> {
                     assertThat(context)
                             .hasBean("applicationServiceMessageSource")
                             .getBean("applicationServiceMessageSource")
                             .hasFieldOrPropertyWithValue("source", "I18nAutoConfigurationTests");
-
 
                     assertThat(context).getBeans(ServiceMessageSourceFactoryBean.class).hasSizeGreaterThanOrEqualTo(1);
 
