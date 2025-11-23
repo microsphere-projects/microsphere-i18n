b3a37f1d5fda53a9b05aab1d11e1760f36361458
Mercy Ma <mercyblitz@gmail.com>
2025-11-23 12:12:08 +0800

    Add utility methods to find PropertySourcesServiceMessageSource beans
    Introduces static methods to locate all PropertySourcesServiceMessageSource beans from a ListableBeanFactory or a collection of ServiceMessageSources, with optional predicate filtering. This enhances bean discovery and filtering capabilities for i18n message sources in Spring contexts.

--- microsphere-i18n-spring/src/main/java/io/microsphere/i18n/spring/PropertySourcesServiceMessageSource.java
+++ microsphere-i18n-spring/src/main/java/io/microsphere/i18n/spring/PropertySourcesServiceMessageSource.java
@@ -1,8 +1,10 @@
 package io.microsphere.i18n.spring;
 
+import io.microsphere.annotation.Nonnull;
 import io.microsphere.i18n.PropertiesResourceServiceMessageSource;
 import io.microsphere.i18n.ReloadableResourceServiceMessageSource;
 import io.microsphere.i18n.ServiceMessageSource;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.env.Environment;
@@ -12,12 +14,19 @@ import org.springframework.lang.Nullable;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import static io.microsphere.collection.Lists.ofList;
+import static io.microsphere.i18n.util.I18nUtils.findAllServiceMessageSources;
 import static io.microsphere.i18n.util.MessageUtils.SOURCE_SEPARATOR;
+import static io.microsphere.lang.function.Predicates.and;
+import static io.microsphere.spring.beans.BeanUtils.getOptionalBean;
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static org.springframework.util.CollectionUtils.isEmpty;
 import static org.springframework.util.StringUtils.hasText;
 
 /**
@@ -88,5 +97,39 @@ public class PropertySourcesServiceMessageSource extends PropertiesResourceServi
     @Override
     public void setEnvironment(Environment environment) {
         this.environment = environment;
+    }
+
+    /**
+     * Find all {@link PropertySourcesServiceMessageSource} beans from the specified {@link ListableBeanFactory}
+     *
+     * @param beanFactory {@link ListableBeanFactory}
+     * @return non-null
+     */
+    @Nonnull
+    public static List<PropertySourcesServiceMessageSource> findAllPropertySourcesServiceMessageSources(ListableBeanFactory beanFactory) {
+        ServiceMessageSource serviceMessageSource = getOptionalBean(beanFactory, ServiceMessageSource.class);
+        return serviceMessageSource == null ? emptyList() : findAllPropertySourcesServiceMessageSources(findAllServiceMessageSources(serviceMessageSource));
+    }
+
+    /**
+     * Find all {@link PropertySourcesServiceMessageSource} beans from the specified {@link ServiceMessageSource ServiceMessageSources}
+     * filtered by the specified {@link Predicate predicates}
+     *
+     * @param serviceMessageSources {@link ServiceMessageSource ServiceMessageSources}
+     * @param predicates            {@link Predicate predicates}
+     * @return non-null
+     */
+    @Nonnull
+    public static List<PropertySourcesServiceMessageSource> findAllPropertySourcesServiceMessageSources(
+            Collection<ServiceMessageSource> serviceMessageSources,
+            Predicate<? super PropertySourcesServiceMessageSource>... predicates) {
+        if (isEmpty(serviceMessageSources)) {
+            return emptyList();
+        }
+        return serviceMessageSources.stream()
+                .filter(PropertySourcesServiceMessageSource.class::isInstance)
+                .map(PropertySourcesServiceMessageSource.class::cast)
+                .filter(and(predicates))
+                .collect(toList());
     }
 }
